cf5e0f34ce484cf94fb04b9ea7b8b37ea054ef63
Ani Kvikvinia <ani@fora-soft.com>
2023-06-06 18:48:38 +0300

    fix(FSIA-694): fix port

--- services/web-client/vite.config.ts
+++ services/web-client/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,8 @@ export default defineConfig({
   server: {
     port: 3001,
     host: true,
+  },
+  preview: {
+    port: 3001,
   },
 });
